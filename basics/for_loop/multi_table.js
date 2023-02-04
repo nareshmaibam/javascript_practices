d94cb6eb1dfc45d786dce947c3921dfc036decfd
nareshmaibam <nareshhmaibam@gmail.com>
2023-02-04 22:25:23 +0530

    for loop

---
+++ basics/for_loop/multi_table.js
@@ -0,0 +1,13 @@
+let num = 10;
+let result = 0;
+//INCREMENT
+for (let i = 1; i <= 10; i++) {
+    result = num * i;
+    console.log(result)
+}
+
+let num2 = 50;
+//DECREMENT
+for(i = num2; i >= 10; i--){
+    console.log(i);
+}
