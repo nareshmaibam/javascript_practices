ecad854965eec9ac51b93d179be02b66d7f08d96
nareshmaibam <879412039a@gmail.com>
2023-03-11 21:28:04 +0530

    advanced objects operation

--- advanced_js/object.js
+++ advanced_js/object.js
@@ -3,4 +3,26 @@ myInfo["firstName"] = "Naresh";
 myInfo["fastName"] = "Singh";
 myInfo["phoneNumber"] = 9988667788;
 myInfo["address"] = "Manipur";
-console.log(myInfo);
+// console.log(myInfo);
+
+let bioData = [];
+const firstName = ["Sanjit", "Naresh", "Asish"];
+const lastName = ["Lungeli", "Singh", "Sharma"];
+const phoneNumber = ["9988776655", "4455667788", "7766554433"];
+const address = ["Assam", "Manipur", "Meghalaya"];
+
+firstName.forEach((item, index) => {
+  bioData.push({ firstName: item, id: 100 + index + 1 + 9098 });
+});
+lastName.forEach((item, index) => {
+  bioData[index] = { ...bioData[index], lastName: item };
+});
+phoneNumber.forEach((item, index) => {
+  bioData[index] = { ...bioData[index], phoneNumber: item };
+});
+address.forEach((item, index) => {
+  bioData[index] = { ...bioData[index], address: item };
+});
+
+console.log(bioData);
++
